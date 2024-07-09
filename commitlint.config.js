f2c9a50fa4b0d497480e374f5cd9cfc3326e07b8
raulrazo <raulrazox@gmail.com>
2024-07-08 21:04:08 -0700

    first commit

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,26 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'type-empty': [0, 'always'],
+    'subject-empty': [0, 'always'],
     //   TODO Add Scope Enum Here
     // 'scope-enum': [2, 'always', ['yourscope', 'yourscope']],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'chore',
-        'style',
-        'refactor',
-        'ci',
-        'test',
-        'perf',
-        'revert',
-        'vercel',
-      ],
-    ],
+    // 'type-enum': [
+    //   2,
+    //   'always',
+    //   [
+    //     'feat',
+    //     'fix',
+    //     'docs',
+    //     'chore',
+    //     'style',
+    //     'refactor',
+    //     'ci',
+    //     'test',
+    //     'perf',
+    //     'revert',
+    //     'vercel',
+    //   ],
+    // ],
   },
 };
